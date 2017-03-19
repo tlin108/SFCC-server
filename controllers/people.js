9527882614d646a9e9319cc091223d46b9b1db0e
tlin108 <tlin108@hotmail.com>
2017-03-18 22:59:47 -0400

    finish route handling

--- controllers/people.js
+++ controllers/people.js
@@ -1,46 +1,4 @@
-const people = [{
-  "id": 1,
-  "name": "Antonio",
-  "favoriteCity": "Néa Ionía"
-}, {
-  "id": 2,
-  "name": "Sandra",
-  "favoriteCity": "New York"
-}, {
-  "id": 3,
-  "name": "Harry",
-  "favoriteCity": "Bom Jesus da Lapa"
-}, {
-  "id": 4,
-  "name": "Helen",
-  "favoriteCity": "New York"
-}, {
-  "id": 5,
-  "name": "Robin",
-  "favoriteCity": "Čelopek"
-}, {
-  "id": 6,
-  "name": "Johnny",
-  "favoriteCity": "New York"
-}, {
-  "id": 7,
-  "name": "Diane",
-  "favoriteCity": "Benhong"
-}, {
-  "id": 8,
-  "name": "Victor",
-  "favoriteCity": "New York"
-}, {
-  "id": 9,
-  "name": "Ernest",
-  "favoriteCity": "New York"
-}, {
-  "id": 10,
-  "name": "Shirley",
-  "favoriteCity": "New York"
-}]
-
-var People = require('../models/people');
+var Peoples = require('../models/people');
 
 module.exports = function(router, app) {
 
@@ -50,31 +8,44 @@ module.exports = function(router, app) {
 
   // display the list of people
   router.get('/people', function (req, res) {
-    People.find({}).
+    Peoples.find({}).
       sort({ createdAt: -1 }).
-      exec(function(err, posts){
+      exec(function(err, people){
         if(err)
           throw err;
-        res.json(posts);
+        res.json(people);
       });  
   });
 
   // add new person to the list of people
   router.post('/people', function (req, res) {
-    var newPerson = new People({
+    var newPeople = new Peoples({
       name: req.body.name,
       favoriteCity: req.body.favoriteCity
     });
-    newPerson.save(function(err){
+    newPeople.save(function(err){
       if(err)
         throw err;
-      res.send('Success');    
+      res.json(newPeople);    
     });
+  });
+
+  // update the person with given ID from the list of people
+  router.put('/people', function (req, res) {
+    Peoples.update({
+      _id: req.body._id
+    }, {
+      favoriteCity: req.body.favoriteCity
+    }, function(err){
+      if(err)
+        throw err;
+      res.send('Updated User with Id ' + req.params.id);
+    })
   });
 
   // get a person's info with given id
   router.get('/people/:id', function (req, res) {
-    People.findById({
+    Peoples.findById({
       _id: req.params.id
     }, function (err, people){
       if(err)
@@ -85,12 +56,12 @@ module.exports = function(router, app) {
 
   // delete a person's info with given id
   router.delete('/people/:id', function (req, res) {
-    People.remove({
+    Peoples.remove({
       _id: req.params.id
-    }, function(err,post){
+    }, function(err){
       if(err)
         throw err;
-      res.json('Deleted!')
+      res.send("Delete People with Id " + req.params.id);
     })
   });
 
